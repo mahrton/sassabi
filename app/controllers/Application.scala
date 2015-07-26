4f7091e96aee7eabcf576757452b0e20cf24cfd9
Marton Tatai <marton.tatai@kpn.com>
2015-07-26 19:13:17 +0200

    initial commit with customised header and menu

---
+++ app/controllers/Application.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller with XhrActionSupport {
+
+  def index = indexWithPath("/")
+
+  def indexWithPath(path: String) = Action {
+    Ok(views.html.main("AAA"))
+  }
+}
